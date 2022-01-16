ae3b09df808f4b3fe0b4e61349ca03b82acc4760
Rubenennj <57841770+Rubenennj@users.noreply.github.com>
2022-01-16 13:30:19 +0100

    fixed memory leak

--- src/functions/sleep.ts
+++ src/functions/sleep.ts
@@ -1,6 +1,6 @@
 import { setTimeout } from "timers/promises"
 
-export const INT32_MAX: number = (1 << 31) >>> 0 - 1 
+export const INT32_MAX: number = ((1 << 31) >>> 0) - 1
 
 export default async function sleep(ms: number): Promise<void> {
     if (ms > INT32_MAX) {
